a2ee256d86824cdad3f6d27d73d6f0d685d88f7e
Sean Sundberg <seansund@us.ibm.com>
2021-07-14 23:50:26 -0500

    Removes nonsensitive function call
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- main.tf
+++ main.tf
@@ -19,7 +19,7 @@ resource null_resource setup_gitops {
     command = "${path.module}/scripts/setup-gitops.sh 'cluster' '${local.yaml_dir}' 'cluster' '${local.application_branch}' '${local.config_namespace}'"
 
     environment = {
-      GIT_CREDENTIALS = jsonencode(nonsensitive(var.git_credentials))
+      GIT_CREDENTIALS = jsonencode(var.git_credentials)
       GITOPS_CONFIG = jsonencode(local.layer_config)
     }
   }
