eb737fb71842c654328f1df64db755cb712c1f0d
Sean Sundberg <seansund@us.ibm.com>
2022-01-19 22:31:48 -0600

    Adds debug flag for destroy
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- main.tf
+++ main.tf
@@ -47,7 +47,7 @@ resource null_resource setup_gitops {
 
   provisioner "local-exec" {
     when = destroy
-    command = "${self.triggers.bin_dir}/igc gitops-module '${self.triggers.name}' -n '${self.triggers.namespace}' --delete --contentDir '${self.triggers.yaml_dir}' --serverName '${self.triggers.server_name}' -l '${self.triggers.layer}'"
+    command = "${self.triggers.bin_dir}/igc gitops-module '${self.triggers.name}' -n '${self.triggers.namespace}' --delete --contentDir '${self.triggers.yaml_dir}' --serverName '${self.triggers.server_name}' -l '${self.triggers.layer}' --debug"
 
     environment = {
       GIT_CREDENTIALS = nonsensitive(self.triggers.git_credentials)
