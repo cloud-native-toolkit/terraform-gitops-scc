8079e3cfef753cdaa8a0e55fd0abd6ffeeec9818
Cloud-Native Toolkit <cloudnativetoolkit@gmail.com>
2022-02-10 14:13:55 -0600

    Fixes outputs
    Signed-off-by: Cloud-Native Toolkit <cloudnativetoolkit@gmail.com>

--- outputs.tf
+++ outputs.tf
@@ -1,32 +1,38 @@
 
 output "name" {
   description = "The name of the module"
-  value       = gitops_module.module.name
+  value       = local.name
+  depends_on  = [gitops_module.module]
 }
 
 output "branch" {
   description = "The branch where the module config has been placed"
-  value       = gitops_module.module.branch
+  value       = local.application_branch
+  depends_on  = [gitops_module.module]
 }
 
 output "namespace" {
   description = "The namespace where the module will be deployed"
-  value       = gitops_module.module.namespace
+  value       = var.namespace
+  depends_on  = [gitops_module.module]
 }
 
 output "server_name" {
   description = "The server where the module will be deployed"
-  value       = gitops_module.module.server_name
+  value       = var.server_name
+  depends_on  = [gitops_module.module]
 }
 
 output "layer" {
   description = "The layer where the module is deployed"
-  value       = gitops_module.module.layer
+  value       = local.layer
+  depends_on  = [gitops_module.module]
 }
 
 output "type" {
   description = "The type of module where the module is deployed"
-  value       = gitops_module.module.type
+  value       = "base"
+  depends_on  = [gitops_module.module]
 }
 
 output "group" {
