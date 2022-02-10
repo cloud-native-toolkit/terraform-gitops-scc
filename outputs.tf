78d4c87bfb5df51e182eb552102ae8cfa8be2d6e
Cloud-Native Toolkit <cloudnativetoolkit@gmail.com>
2022-02-10 14:11:19 -0600

    Converts to use gitops provider
    Signed-off-by: Cloud-Native Toolkit <cloudnativetoolkit@gmail.com>

--- outputs.tf
+++ outputs.tf
@@ -1,48 +1,42 @@
 
 output "name" {
   description = "The name of the module"
-  value       = local.name
-  depends_on  = [null_resource.setup_gitops]
+  value       = gitops_module.module.name
 }
 
 output "branch" {
   description = "The branch where the module config has been placed"
-  value       = local.application_branch
-  depends_on  = [null_resource.setup_gitops]
+  value       = gitops_module.module.branch
 }
 
 output "namespace" {
   description = "The namespace where the module will be deployed"
-  value       = var.namespace
-  depends_on  = [null_resource.setup_gitops]
+  value       = gitops_module.module.namespace
 }
 
 output "server_name" {
   description = "The server where the module will be deployed"
-  value       = var.server_name
-  depends_on  = [null_resource.setup_gitops]
+  value       = gitops_module.module.server_name
 }
 
 output "layer" {
   description = "The layer where the module is deployed"
-  value       = local.layer
-  depends_on  = [null_resource.setup_gitops]
+  value       = gitops_module.module.layer
 }
 
 output "type" {
   description = "The type of module where the module is deployed"
-  value       = "base"
-  depends_on  = [null_resource.setup_gitops]
+  value       = gitops_module.module.type
 }
 
 output "group" {
   description = "The value of the group flag indicating the scc was created for all service accounts in the group"
   value       = var.group || var.service_account == ""
-  depends_on  = [null_resource.setup_gitops]
+  depends_on  = [gitops_module.module]
 }
 
 output "service_account" {
   description = "The name of the service account"
   value       = local.service_account
-  depends_on  = [null_resource.setup_gitops]
+  depends_on  = [gitops_module.module]
 }
